aca6ed905a173a644fa7dc853f36a8d2410c61ea
BagaevDanil <MrBagaevDanil@yandex.ru>
2023-04-23 11:18:02 +0300

    added test for TreeAVL

--- test/main.cpp
+++ test/main.cpp
@@ -2,8 +2,80 @@
 #include <gtest/gtest.h>
 #include "../src/vector.h"
 #include "../src/queue.h"
+#include "../src/treeAVL.h"
 
 using namespace std;
+
+TEST(TreeAVL, Insert1) {
+    TTreeAVL<int, int> tree;
+    int arr[] = { 8, 11, 4, 1, 10, 7, 3, 15, 12, 5, 2, 6, 13, 9, 14 };
+    int size = 15;
+
+    for (int i = 0; i < size; i++) {
+        tree.Insert(arr[i], i * arr[i]);
+    }
+
+    ASSERT_EQ(tree.GetSize(), size);
+    ASSERT_TRUE(tree.GetHeightTree() <= 4);
+}
+
+TEST(TreeAVL, Insert2) {
+    TTreeAVL<int, int> tree;
+    int arr[] = { 8, 11, 4, 1, 10, 7, 3, 15, 12, 5, 2, 6, 13, 9, 14 };
+    int size = 15;
+
+    for (int i = 0; i < size; i++) {
+        tree.Insert(arr[i], i * arr[i]);
+    }
+
+    ASSERT_THROW(tree.Insert(1, 0), exception);
+}
+
+TEST(TreeAVL, Remove1) {
+    TTreeAVL<int, int> tree;
+    int arr[] = { 16, 8, 11, 4, 1, 10, 7, 3, 15, 12, 5, 2, 6, 13, 17, 18, 9, 14 };
+    int size = 18;
+
+    for (int i = 0; i < size; i++) {
+        tree.Insert(arr[i], i * arr[i]);
+    }
+
+    tree.Remove(1);
+    tree.Remove(7);
+    tree.Remove(4);
+    tree.Remove(13);
+
+    ASSERT_EQ(tree.GetSize(), size - 4);
+    ASSERT_TRUE(tree.GetHeightTree() <= 4);
+    ASSERT_THROW(tree.GetValue(7), exception);
+    ASSERT_EQ(tree.GetValue(16), 0);
+}
+
+TEST(TreeAVL, Remove2) {
+    TTreeAVL<int, int> tree;
+    int arr[] = { 16, 8, 11, 4, 1, 10, 7, 3, 15, 12, 5, 2, 6, 13, 17, 18, 9, 14 };
+    int size = 18;
+
+    for (int i = 0; i < size; i++) {
+        tree.Insert(arr[i], i * arr[i]);
+    }
+
+    ASSERT_THROW(tree.Remove(200), exception);
+}
+
+TEST(TreeAVL, GetValue) {
+    TTreeAVL<int, int> tree;
+    int arr[] = { 16, 8, 11, 4, 1, 10, 7, 3, 15, 12, 5, 2, 6, 13, 17, 18, 9, 14 };
+    int size = 18;
+
+    for (int i = 0; i < size; i++) {
+        tree.Insert(arr[i], i * arr[i]);
+    }
+
+    for (int i = 0; i < size; i++) {
+        ASSERT_EQ(tree.GetValue(arr[i]), i * arr[i]);
+    }
+}
 
 TEST(Vector, AddBack) {
     int arr[] = { 41, 467, 334, 500, 169, 724, 478 };
